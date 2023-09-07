6fc39f91b8df327d434c6b4d4b1ab1c789b8adc3
Naotsun <51815450+Naotsun19B@users.noreply.github.com>
2023-09-07 19:04:06 +0900

    Moved the development environment to UE5.3

--- Source/RecentAssetsPlugin.Target.cs
+++ Source/RecentAssetsPlugin.Target.cs
@@ -8,7 +8,7 @@ public class RecentAssetsPluginTarget : TargetRules
 	public RecentAssetsPluginTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
 #if UE_5_2_OR_LATER
 		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 #endif
